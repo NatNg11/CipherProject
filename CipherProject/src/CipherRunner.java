f09ce49a27a6c77224da1263ecd1a5ea34e8a459
NatNg11 <nathan.ti.nguyen@gmail.com>
2021-03-09 14:42:03 -0600

    h

---
+++ CipherProject/src/CipherRunner.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class CipherRunner {
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		
+		System.out.println("Would you like to encode or decode (e/d)?");
+		String choice = kb.nextLine().trim();
+		if(!(choice.equals("e")||choice.equals("d"))) {
+			throw new IllegalArgumentException("must enter an e or d");
+		}
+		
+		System.out.println("Enter the text you want to encode/decode.");
+		String text = kb.nextLine().trim();
+		
+		System.out.println("Enter the key you want to use.");
+		String[] k = kb.nextLine().trim().split("");
+		int[] key = new int[k.length];
+		for(int x = 0;x<k.length;x++) {
+			key[x] = Integer.parseInt(k[x])-1;
+		}
+		
+		TranspositionCipher cip = new TranspositionCipher(text,key.length);
+		if(choice.equals("e")) {
+			System.out.println(cip.encode(key));
+		}
+		else if(choice.equals("d")) {
+			cip.decode();
+		}
+	}
+}
---
+++ CipherProject/src/TranspositionCipher.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class TranspositionCipher {
+	char[][] mat;
+	public TranspositionCipher(String text, int len) {
+		if(len>text.length()) { throw new IllegalArgumentException("key is too long"); }
+		mat = new char[(int)(Math.ceil(text.length()/(double)len))][len];
+		
+		int index = 0;
+		for(int x = 0;x<mat.length;x++) {
+			for(int y = 0;y<mat[x].length;y++) {
+				if(index>=text.length()) { mat[x][y] = ' '; } 
+				else {mat[x][y] = text.charAt(index++); }
+			}
+		}
+		
+		for(char[] a : mat) {
+			System.out.println(Arrays.toString(a));
+		}
+	}
+	
+	public String encode(int[] key) {
+		String ret = "";
+		for(int y : key) {
+			for(int x = 0;x<mat.length;x++) {
+				if(mat[x][y]!=' ') {
+					ret += mat[x][y];
+				}
+			}
+		}
+		return ret;
+	}
+	
+	public String decode() {
+		return "";
+	}
+}
